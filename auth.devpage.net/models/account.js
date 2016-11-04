e188dfcfc2f94b8a2642fc4d3d889a34733f3c67
JTKon <kon@devpage.net>
2016-11-04 09:06:22 +0000

    auth.devpage.net test

---
+++ auth.devpage.net/models/account.js
@@ -0,0 +1,9 @@
+var db = require(../mongoDB);
+
+var account = db.model('account', {
+    id : {type: String, required: true},
+    pw : {type: String, required: true},
+    role : {type: String, required: true}
+});
+
+module.exports = account;
